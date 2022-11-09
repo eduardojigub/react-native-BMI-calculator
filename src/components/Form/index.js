a2db23cfc4bc4a68ea1cd9d1fcac708b0c3385e1
eduardojigub <eduardobrunoit@gmail.com>
2022-11-09 14:40:42 -0300

    feat: creating form states

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,7 +1,14 @@
-import React from 'react';
-import { View, Text, TextInput } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button } from 'react-native';
+import ResultIMC from '../ResultIMC';
 
 export default function Form() {
+  const [height, setHeight] = useState(null);
+  const [weight, setWeight] = useState(null);
+  const [messageImc, setMessageImc] = useState('Preencha o peso e altura');
+  const [imc, setImc] = useState(null);
+  const [textButton, setTextButton] = useState('Calculate');
+
   return (
     <View>
       <View>
@@ -9,7 +16,9 @@ export default function Form() {
         <TextInput placeholder="Ex: 1.75" keyboardType="numeric" />
         <Text>Peso</Text>
         <TextInput placeholder="Ex: 80.5kg" keyboardType="numeric" />
+        <Button title="Calculate my BMI" />
       </View>
+      <ResultIMC messageResultImc={messageImc} resultImc={imc} />
     </View>
   );
 }
