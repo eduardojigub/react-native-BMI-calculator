f454c4946f6b23ed7c8b82be4ad7450e04353a41
eduardojigub <eduardobrunoit@gmail.com>
2022-11-09 16:09:30 -0300

    feat: styling form

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
 import ResultIMC from '../ResultIMC';
+import styles from './style';
 
 export default function Form() {
   const [height, setHeight] = useState(null);
   const [weight, setWeight] = useState(null);
-  const [messageImc, setMessageImc] = useState('Preencha o peso e altura');
+  const [messageImc, setMessageImc] = useState('Fill your weight and height');
   const [imc, setImc] = useState(null);
   const [textButton, setTextButton] = useState('Calculate');
 
@@ -28,23 +29,30 @@ export default function Form() {
   };
 
   return (
-    <View>
-      <View>
-        <Text>Altura</Text>
+    <View style={styles.formContext}>
+      <View style={styles.form}>
+        <Text style={styles.formLabel}>Height</Text>
         <TextInput
+          style={styles.input}
           onChangeText={setHeight}
           value={height}
-          placeholder="Ex: 1.75"
+          placeholder="Ex: 1.75 (in meters)"
           keyboardType="numeric"
         />
-        <Text>Peso</Text>
+        <Text style={styles.formLabel}>Weight</Text>
         <TextInput
+          style={styles.input}
           onChangeText={setWeight}
           value={weight}
-          placeholder="Ex: 80.5kg"
+          placeholder="Ex: 80.5kg (in kilos)"
           keyboardType="numeric"
         />
-        <Button onPress={() => validateIMC()} title={textButton} />
+        <TouchableOpacity
+          style={styles.buttonCalculator}
+          onPress={() => validateIMC()}
+        >
+          <Text style={styles.textButtonCalculator}>{textButton}</Text>
+        </TouchableOpacity>
       </View>
       <ResultIMC messageResultImc={messageImc} resultImc={imc} />
     </View>
--- src/components/Form/style.js
+++ src/components/Form/style.js
@@ -1,7 +1,50 @@
-import { Stylesheet } from 'react-native';
+import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-  Form: {},
+  formContext: {
+    width: '100%', // ocupa 100% de largura da tela,
+    height: '100%', //ocupa 100% de altura da tela
+    bottom: 0, //pra ir até o final da tela
+    backgroundColor: '#ffffff',
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    alignItems: 'center',
+    marginTop: 30,
+  },
+  form: {
+    width: '100%',
+    height: 'auto', //conforme novos componentes são adicionados, ele automatiza o processo de posicionamento
+    marginTop: 30,
+    padding: 10,
+  },
+  formLabel: {
+    color: '#000000',
+    fontSize: 18,
+    paddingLeft: 20,
+  },
+  input: {
+    width: '90%',
+    borderRadius: 50,
+    backgroundColor: '#f6f6f6',
+    height: 40,
+    margin: 12,
+    paddingLeft: 10,
+  },
+  buttonCalculator: {
+    borderRadius: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '90%',
+    backgroundColor: '#FF0043',
+    paddingTop: 14,
+    paddingBottom: 14,
+    marginLeft: 12,
+    margintTop: 30,
+  },
+  textButtonCalculator: {
+    fontSize: 20,
+    color: '#ffffff',
+  },
 });
 
 export default styles;
