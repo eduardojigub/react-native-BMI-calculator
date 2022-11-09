af4d84005ee6deebd80d2fe671d8cd9952ab6a8f
eduardojigub <eduardobrunoit@gmail.com>
2022-11-09 17:42:17 -0300

    feat: creating share button and styles

--- src/components/ResultIMC/index.js
+++ src/components/ResultIMC/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, TouchableOpacity, Share } from 'react-native';
 import styles from './style';
 
 export default function ResultIMC(props) {
+  const onShare = async () => {
+    const result = await Share.share({
+      message: 'My BMI today is: ' + props.resultImc,
+    });
+  };
+
   return (
     <View style={styles.resultImc}>
+      <View style={styles.boxSharebutton}>
+        {props.resultImc != null ? (
+          <TouchableOpacity style={styles.shared}>
+            <Text style={styles.sharedText}>Share</Text>
+          </TouchableOpacity>
+        ) : (
+          <View />
+        )}
+      </View>
       <Text style={styles.information}>{props.messageResultImc}</Text>
       <Text style={styles.numberImc}>{props.resultImc}</Text>
     </View>
